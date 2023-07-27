8147f59e936cc1f6967147536dcf15e98ced66c3
Ayoub <ay.belkacem@gmail.com>
2023-07-27 19:19:20 +0100

    Dynamically allocates memory to an int arry based on user input for the array size

--- Day_03/Exercise_02.c
+++ Day_03/Exercise_02.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 /*
 Exercise 2: Dynamic Array Creation 
 Write a C program that dynamically allocates memory for an integer array based on user input for the array size.
 */
 
+int main(void){
+    int arrsize = 0;
+    int *ptr;
+    printf("Enter the size of Array:\n");
+    scanf("%d", &arrsize);
+    ptr = (int *)malloc(arrsize * sizeof(int));
+
+if (ptr == NULL) {
+        printf("Memory allocation failed. Exiting program.");
+        return 1;
+    }
+
+    printf("The allocated memory is: %ld bytes\n", arrsize * sizeof(int));
+    printf("The adress of the allocated memory is: %p\n", ptr);
+
+free (ptr);
+return (0);
+}
