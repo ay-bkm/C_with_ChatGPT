0f00fd8a60f6e72d79032d368d0c1cfcefe500ce
Ayoub <ay.belkacem@gmail.com>
2023-07-28 03:57:27 +0100

    Day 03

---
+++ Day_03/Exercise_03.c
@@ -0,0 +1,13 @@
+
+
+/*
+TODO: Exercise 3: Using Pointers to Modify Array Elements
+Write a C function that takes an integer array 
+and its size as parameters 
+and increments all elements of the array by a specified value.
+*/
+
+void incremFunc(int arr[], int size, int n)
+{
+	
+}
